2d1526af8a7c19498e5b2490c2be6a40f8e7e079
fiendish <patcherton.fixesthings@gmail.com>
2015-11-25 18:38:05 +0100

    update comment, clarify name

--- rename_android_images.sh
+++ rename_android_images.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+#
 # Android phone camera keeps naming files stupidly. 
 # File type already dictates that a video is a video.
+# This makes it easier to sort by capture date online without relying on EXIF data that doesn't exist in all files.
+
 
 for f in $1/*
 do
