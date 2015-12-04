25c93b70c4ec2ff7bed54b928a198cc88e8d8f29
fiendish <patcherton.fixesthings@gmail.com>
2015-12-04 15:01:33 +0100

    preserve type identifier

--- rename_android_images.sh
+++ rename_android_images.sh
@@ -4,10 +4,17 @@
 # File type already dictates that a video is a video.
 # This makes it easier to sort by capture date online without relying on EXIF data that doesn't exist in all files.
 
+function move_prefix {
+   mv -n "$2" "`echo $2 | sed -r 's/('$1')_(.*)(\....)$/\2_\1\3/'`"
+}
 
-for f in $1/*
-do
-   mv -n $f ${f/IMG_/}
-   mv -n $f ${f/VID_/}
-   mv -n $f ${f/PANO_/}
-done
+function move_all_prefix {
+   for f in $1/$2*
+   do
+      move_prefix "$2" "$f"
+   done
+}
+
+move_all_prefix "$1" "IMG"
+move_all_prefix "$1" "VID"
+move_all_prefix "$1" "PANO"
