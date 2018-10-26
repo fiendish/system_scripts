f3442a44aa78838489a2d6f2e68b5df74af16ef3
Avi Kelman (fiendish) <patcherton.fixesthings@gmail.com>
2018-10-26 12:41:53 -0400

    Create lemme_unplug.sh

---
+++ mac/lemme_unplug.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Lets you unplug from power while your mac laptop is in clamshell mode without it going to sleep
+
+function finish {
+    sudo pmset -b disablesleep 0
+    echo ""
+    echo "Computer WILL sleep now on a lid close event."
+}
+trap finish EXIT
+
+echo "Disabling sleep requires an administrator password."
+sudo pmset -b disablesleep 1 && echo "Computer will NOT sleep now on a lid close event." && echo "You can now unplug safely and then press CTRL+C after." && while true; do sleep 10000; done
